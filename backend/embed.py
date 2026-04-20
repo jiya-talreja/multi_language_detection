eccf6e75c4c355e258d26f2fe038103538240313
Noushad Hamid <noushadhamid49@gmail.com>
2026-04-20 10:21:42 +0530

    Update embed.py

--- backend/embed.py
+++ backend/embed.py
@@ -1,23 +1,24 @@
 import numpy as np
 from sentence_transformers import SentenceTransformer
 
-# Global model instance
 _model = None
 
 def get_model(model_name: str):
     global _model
     if _model is None:
-        print(f"Loading model into memory: {model_name}")
+        print(f"Loading model: {model_name}")
         _model = SentenceTransformer(model_name)
     return _model
 
-def encode_dataset(texts: list[str], model_name: str = "paraphrase-multilingual-MiniLM-L12-v2", batch_size: int = 32):
-    """
-    Encodes a list of strings into embeddings.
-    """
+
+def encode_dataset(texts: list[str], model_name: str, batch_size: int = 32):
     model = get_model(model_name)
-    
+    print("MODEL : ",model)
     print(f"Encoding {len(texts)} texts...")
-    embeddings = model.encode(texts, batch_size=batch_size, show_progress_bar=True)
-    
-    return embeddings
+    embeddings = model.encode(
+        texts,
+        batch_size=batch_size,
+        show_progress_bar=True
+    )
+
+    return np.array(embeddings)
